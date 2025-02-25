6051ea9e102ee2c2fcf672580eb214210f075d2a
BolognaGov <abeedammer1234@gmail.com>
2025-02-25 20:01:34 +0300

    start animations when all resources are loaded

--- js/animation.js
+++ js/animation.js
@@ -3,16 +3,23 @@
 let imgs = ["mohe.jpg", "bauc.jpg"];
 let index = 0;
 
-setInterval(() => {
-    let imgElement = document.querySelector(".icon-container img");
-    
-    // Change the image when opacity is 0 (25% and 75% of the animation cycle)
-    setTimeout(() => {
-        index = (index + 1) % imgs.length;
-        if (index == 0) {
-            imgElement.src = imgElement.src.replace("bauc.jpg", imgs[index]);
-        } else {
-            imgElement.src = imgElement.src.replace("mohe.jpg", imgs[index]);
-        }
-    }, 2500);
-}, 5000);
+function startSwapAnimation() {
+    setInterval(() => {
+        let imgElement = document.querySelector(".icon-container img");
+        
+        // Change the image when opacity is 0 (25% and 75% of the animation cycle)
+        setTimeout(() => {
+            index = (index + 1) % imgs.length;
+            if (index == 0) {
+                imgElement.src = imgElement.src.replace("bauc.jpg", imgs[index]);
+            } else {
+                imgElement.src = imgElement.src.replace("mohe.jpg", imgs[index]);
+            }
+        }, 2500);
+    }, 5000);
+}
+
+// code to run when page is fully loaded (including images)
+window.onload = function() {
+    startSwapAnimation();
+};
